c9a3f4173d59b64568cdd877bd67e1c78e852939
erezrokah <erezrokah@users.noreply.github.com>
2022-05-12 15:20:30 +0300

    refactor: Simplify queries

--- queries/compute/oslogin_disabled.sql
+++ queries/compute/oslogin_disabled.sql
@@ -1,5 +1,4 @@
 SELECT project_id, name, self_link AS link
 FROM gcp_compute_projects
-WHERE common_instance_metadata_items IS NULL
-    OR common_instance_metadata_items ->> 'enable-oslogin' IS NULL
+WHERE common_instance_metadata_items ->> 'enable-oslogin' IS NULL
     OR NOT common_instance_metadata_items ->> 'enable-oslogin' = ANY ('{1,true,True,TRUE,y,yes}');
--- queries/compute/serial_port_connection_enabled.sql
+++ queries/compute/serial_port_connection_enabled.sql
@@ -1,4 +1,3 @@
 SELECT project_id, name, self_link AS link
 FROM gcp_compute_instances
-WHERE metadata_items IS NOT NULL
-    AND metadata_items ->> 'serial-port-enable' = ANY ('{1,true,True,TRUE,y,yes}');
+WHERE metadata_items ->> 'serial-port-enable' = ANY ('{1,true,True,TRUE,y,yes}');

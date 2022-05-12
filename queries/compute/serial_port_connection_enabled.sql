fbd16383459992f0007563fc7cd3f0ada6d3410b
erezrokah <erezrokah@users.noreply.github.com>
2022-05-12 14:39:11 +0300

    fix: Add all working variants

--- queries/compute/serial_port_connection_enabled.sql
+++ queries/compute/serial_port_connection_enabled.sql
@@ -1,4 +1,4 @@
 SELECT project_id, name, self_link AS link
 FROM gcp_compute_instances
 WHERE metadata_items IS NOT NULL
-    AND metadata_items ->> 'serial-port-enable' = ANY ('{1,true,True,TRUE}');
+    AND metadata_items ->> 'serial-port-enable' = ANY ('{1,true,True,TRUE,y,yes}');

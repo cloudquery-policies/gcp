08837d431d77c8dd8b650b11afac9f9f3e8c5864
Erez Rokah <erezrokah@users.noreply.github.com>
2022-05-12 15:22:57 +0300

    fix: Support true string variants (#24)
    Fixes https://github.com/cloudquery-policies/gcp/issues/19

--- queries/compute/instances_without_block_project_wide_ssh_keys.sql
+++ queries/compute/instances_without_block_project_wide_ssh_keys.sql
@@ -1,4 +1,4 @@
 SELECT project_id, name, self_link AS link
 FROM gcp_compute_instances
-WHERE metadata_items IS NULL OR metadata_items ->> 'block-project-ssh-keys' IS NULL
-    OR metadata_items ->> 'block-project-ssh-keys' != 'true';
+WHERE metadata_items ->> 'block-project-ssh-keys' IS NULL
+    OR NOT metadata_items ->> 'block-project-ssh-keys' = ANY ('{1,true,True,TRUE,y,yes}');
